9f9a8dca74201e6522c6c7510d5fd28d2a120370
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-06-01 15:59:19 -0300

    rota de perfis através do ID

--- perfis/models.py
+++ perfis/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Perfil(object):
+    def __init__(self, nome='',email='', telefone='', nome_da_empresa=''):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+        self.nome_da_empresa = nome_da_empresa
--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from perfis.models import Perfil
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if (perfil_id == 1):
+        perfil = Perfil("Fulano",'@teste.com','333333','IFB')
+
+    if (perfil_id == 2):
+        perfil = Perfil("Siclano",'@dama.com','777777','UNG')
+   
+    return render(request, 'perfil.html', {'perfil': perfil})    
