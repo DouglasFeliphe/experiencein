d0c6abd086b4e20c1b6da33ecdc2353e45c883d9
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-06-09 15:56:22 -0300

    Persistencia do dados

--- perfis/views.py
+++ perfis/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render
 from perfis.models import Perfil
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'perfis': Perfil.objects.all()})
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
+    perfil = Perfil.objects.get(id=perfil_id)
 
-    if (perfil_id == 1):
-        perfil = Perfil("Fulano",'@teste.com','333333','IFB')
-
-    if (perfil_id == 2):
-        perfil = Perfil("Siclano",'@dama.com','777777','UNG')
-   
     return render(request, 'perfil.html', {'perfil': perfil})    
+
+def convidar(request, perfil):
+    pass 
