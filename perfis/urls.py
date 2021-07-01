563b93dec0d6f2f097fd61c7bf67d5797db0b18f
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-07-01 05:41:10 -0300

    Aceitando convites e listando contatos

--- perfis/urls.py
+++ perfis/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
     path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar'),
+    path('perfis/<int:convite_id>/aceitar',views.aceitar, name='aceitar')
 ] 
--- perfis/views.py
+++ perfis/views.py
@@ -1,23 +1,34 @@
-from django.shortcuts import render
-from perfis.models import Perfil
+from django.shortcuts import redirect, render
+from perfis.models import Convite, Perfil
 # Create your views here.
 def index(request):
     return render(request,'index.html',{'perfis': Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
+
 def exibir(request, perfil_id):
 
     perfil = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    ja_e_contato = perfil in perfil_logado.contatos.all()
+    return render(request,'perfil.html',{'perfil':perfil,'perfil_logado':perfil_logado,'ja_e_contato':ja_e_contato})
 
-    return render(request, 'perfil.html', {'perfil': perfil,'perfil_logado': get_perfil_logado(request)})    
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
-    pperfil_logado.convidar(perfil_a_convidar)
+    perfil_logado.convidar(perfil_a_convidar)
+
 
 def get_perfil_logado(request):   
     return Perfil.objects.get(id=3)
-    
+
+
+def aceitar(request,convite_id):
+    convite = Convite.objects.get(id=convite_id) 
+    convite.aceitar()  
+    return redirect('index')
+
+
       
 
       
